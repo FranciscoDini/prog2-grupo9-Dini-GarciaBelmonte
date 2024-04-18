a210eccb3d9aeee813638553793cd865d91e4b5f
Fermin Garcia Belmonte <NenoGB@Air-de-Luis.fibertel.com.ar>
2024-04-18 15:35:41 -0300

    agruegue los comentarios, modifique los headers

--- proyectoDiniGarciaB/db/datos.js
+++ proyectoDiniGarciaB/db/datos.js
@@ -7,13 +7,242 @@ const datos = {
         nroDoc : 46647592,
         imagen : "/images/users/elon.jpg"
     },
-    productos : [
+    productos: [
         {
-            nombre : "samsung smart tv",
-            imagen : "/images/img-tv-samsung-smart.jpg", 
-            descripcion : "television samsung smart tv 15 inch display"
+            nombre: "Camiseta del Real Madrid 18/19",
+            id: "1",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta del Real Madrid edición 2018/2019",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
         },
+        {
+            nombre: "Camiseta del Barcelona 20/21",
+            id: "2",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta del Barcelona edición 2020/2021",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Camiseta del Bayern Munich 16/17",
+            id: "3",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta del Bayern Munich edición 2016/2017",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Camiseta PSG 21/22",
+            id: "4",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta oficial del Paris Saint-Germain para la temporada 2021/2022",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Camiseta Juventus 20/21",
+            id: "5",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta oficial de la Juventus FC para la temporada 2020/2021",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Camiseta Manchester City 19/20",
+            id: "6",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta oficial del Manchester City para la temporada 2019/2020",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Camiseta AC Milan 18/19",
+            id: "7",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta oficial del AC Milan para la temporada 2018/2019",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Camiseta Borussia Dortmund 21/22",
+            id: "8",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta oficial del Borussia Dortmund para la temporada 2021/2022",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Camiseta Chelsea 20/21",
+            id: "9",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta oficial del Chelsea FC para la temporada 2020/2021",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Camiseta Barcelona 17/18",
+            id: "10",
+            imagen: "/images/img-tv-samsung-smart.jpg",
+            descripcion: "Camiseta oficial del FC Barcelona para la temporada 2017/2018",
+            comentarios: [
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                },
+                {
+                    nombre: "fdini",
+                    texto: "Son de muy buena calidad y tienen una gran cantidad de talles",
+                    imagen: "/images/img-tv-samsung-smart.jpg"
+                }
+            ]
+        }
     ]
 };
-
 module.exports = datos
