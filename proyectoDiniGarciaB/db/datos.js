8b4462552b216c0d533487253d568008c0984781
Fermin Garcia Belmonte <NenoGB@MacBook-Air-de-Luis.local>
2024-04-19 13:26:39 -0300

    agruegue las imagenes y le di estilo al css

--- proyectoDiniGarciaB/db/datos.js
+++ proyectoDiniGarciaB/db/datos.js
@@ -11,7 +11,7 @@ const datos = {
         {
             nombre: "Camiseta del Real Madrid 18/19",
             id: "1",
-            imagen: "/images/products/img-tv-samsung-smart.jpg",
+            imagen: "/images/products/camiseta-realmadrid-19:20.jpg",
             descripcion: "Camiseta del Real Madrid edición 2018/2019",
             comentarios: [
                 {
@@ -34,7 +34,7 @@ const datos = {
         {
             nombre: "Camiseta del Barcelona 20/21",
             id: "2",
-            imagen: "/images/products/img-tv-samsung-smart.jpg",
+            imagen: "/images/products/barcelona-camiseta-20-21.jpg",
             descripcion: "Camiseta del Barcelona edición 2020/2021",
             comentarios: [
                 {
@@ -57,7 +57,7 @@ const datos = {
         {
             nombre: "Camiseta del Bayern Munich 16/17",
             id: "3",
-            imagen: "/images/products/img-tv-samsung-smart.jpg",
+            imagen: "/images/products/camiseta-bayern-16:17.jpg",
             descripcion: "Camiseta del Bayern Munich edición 2016/2017",
             comentarios: [
                 {
@@ -80,7 +80,7 @@ const datos = {
         {
             nombre: "Camiseta PSG 21/22",
             id: "4",
-            imagen: "/images/products/img-tv-samsung-smart.jpg",
+            imagen: "/images/products/camiseta-psg-21:22.jpg",
             descripcion: "Camiseta oficial del Paris Saint-Germain para la temporada 2021/2022",
             comentarios: [
                 {
@@ -103,7 +103,7 @@ const datos = {
         {
             nombre: "Camiseta Juventus 20/21",
             id: "5",
-            imagen: "/images/products/img-tv-samsung-smart.jpg",
+            imagen: "/images/products/camiseta-juventus-21:22.jpg",
             descripcion: "Camiseta oficial de la Juventus FC para la temporada 2020/2021",
             comentarios: [
                 {
@@ -126,7 +126,7 @@ const datos = {
         {
             nombre: "Camiseta Manchester City 19/20",
             id: "6",
-            imagen: "/images/products/img-tv-samsung-smart.jpg",
+            imagen: "/images/products/camiseta-city-21:22.jpg",
             descripcion: "Camiseta oficial del Manchester City para la temporada 2019/2020",
             comentarios: [
                 {
@@ -149,7 +149,7 @@ const datos = {
         {
             nombre: "Camiseta AC Milan 18/19",
             id: "7",
-            imagen: "/images/products/img-tv-samsung-smart.jpg",
+            imagen: "/images/products/camiseta-milan-19:20.jpg",
             descripcion: "Camiseta oficial del AC Milan para la temporada 2018/2019",
             comentarios: [
                 {
@@ -172,7 +172,7 @@ const datos = {
         {
             nombre: "Camiseta Borussia Dortmund 21/22",
             id: "8",
-            imagen: "/images/products/img-tv-samsung-smart.jpg",
+            imagen: "/images/products/camiseta-dortmund-21:22.jpg",
             descripcion: "Camiseta oficial del Borussia Dortmund para la temporada 2021/2022",
             comentarios: [
                 {
