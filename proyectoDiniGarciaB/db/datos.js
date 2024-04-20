a3df31132f57ccd3550675f176ea7f29466dab10
francisco_dini <21franciscodini@gmail.com>
2024-04-20 20:48:14 -0300

    Tipografias y correcciones finales

--- proyectoDiniGarciaB/db/datos.js
+++ proyectoDiniGarciaB/db/datos.js
@@ -9,7 +9,7 @@ const datos = {
     },
     productos: [
         {
-            nombre: "Camiseta del Real Madrid 18/19",
+            nombre: "Real Madrid 18/19",
             id: "1",
             imagen: "/images/products/camiseta-realmadrid-19-20.jpg",
             descripcion: "Camiseta del Real Madrid edición 2018/2019",
@@ -32,7 +32,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta del Barcelona 20/21",
+            nombre: "Barcelona 20/21",
             id: "2",
             imagen: "/images/products/barcelona-camiseta-20-21.jpg",
             descripcion: "Camiseta del Barcelona edición 2020/2021",
@@ -55,7 +55,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta del Bayern Munich 16/17",
+            nombre: "Bayern Munich 16/17",
             id: "3",
             imagen: "/images/products/camiseta-bayern-16-17.jpg",
             descripcion: "Camiseta del Bayern Munich edición 2016/2017",
@@ -78,7 +78,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta PSG 21/22",
+            nombre: "PSG 21/22",
             id: "4",
             imagen: "/images/products/camiseta-psg-21-22.jpg",
             descripcion: "Camiseta oficial del Paris Saint-Germain para la temporada 2021/2022",
@@ -101,7 +101,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta Juventus 20/21",
+            nombre: "Juventus 20/21",
             id: "5",
             imagen: "/images/products/camiseta-juventus-21-22.jpg",
             descripcion: "Camiseta oficial de la Juventus FC para la temporada 2020/2021",
@@ -124,7 +124,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta Manchester City 19/20",
+            nombre: "Manchester City 19/20",
             id: "6",
             imagen: "/images/products/camiseta-city-21-22.jpg",
             descripcion: "Camiseta oficial del Manchester City para la temporada 2019/2020",
@@ -147,7 +147,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta AC Milan 18/19",
+            nombre: "AC Milan 18/19",
             id: "7",
             imagen: "/images/products/camiseta-milan-19-20.jpg",
             descripcion: "Camiseta oficial del AC Milan para la temporada 2018/2019",
@@ -170,7 +170,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta Borussia Dortmund 21/22",
+            nombre: "Borussia Dortmund 21/22",
             id: "8",
             imagen: "/images/products/camiseta-dortmund-21-22.jpg",
             descripcion: "Camiseta oficial del Borussia Dortmund para la temporada 2021/2022",
@@ -193,7 +193,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta Chelsea 20/21",
+            nombre: "Chelsea 20/21",
             id: "9",
             imagen: "/images/products/camiseta-chelsea-20-21.jpg",
             descripcion: "Camiseta oficial del Chelsea FC para la temporada 2020/2021",
@@ -216,7 +216,7 @@ const datos = {
             ]
         },
         {
-            nombre: "Camiseta Barcelona 17/18",
+            nombre: "Barcelona 17/18",
             id: "10",
             imagen: "/images/products/camiseta-barcelona-17-18.jpg",
             descripcion: "Camiseta oficial del FC Barcelona para la temporada 2017/2018",
