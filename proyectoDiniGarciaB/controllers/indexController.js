3c452f2fee280d62aa8cb2bfb42f9eca5e812d46
francisco_dini <21franciscodini@gmail.com>
2024-05-27 11:12:28 -0300

    index controller

--- proyectoDiniGarciaB/controllers/indexController.js
+++ proyectoDiniGarciaB/controllers/indexController.js
@@ -1,9 +1,17 @@
-const datos = require('../database/models/index')
+const datos = require('../database/models')
 
 const controller = {
-    index : function(req, res){
-        res.render('index', {datos : datos});
-      }
+  index: function (req, res) {
+    datos.Producto.findAll()
+    .then(function (results) {
+      /*return res.send(results)*/
+      return res.render('index', {datos:results})
+    })
+    .catch(function (error) {
+      return console.log(error);;
+    })
+
+  }
 };
 
 
