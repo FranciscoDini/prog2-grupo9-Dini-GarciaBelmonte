ebe5c4472afd276592055a2c5b8caad51856959f
Fermin Garcia Belmonte <NenoGB@MacBook-Air-de-Luis.local>
2024-06-12 15:42:04 -0300

    express-validator y sus funciones

--- proyectoDiniGarciaB/controllers/productsController.js
+++ proyectoDiniGarciaB/controllers/productsController.js
@@ -21,38 +21,7 @@ const controller = {
     
     },
     search : (req,res)=>{
-      let form = req.body;
-        return res.send(form)
-        let filtro = {
-            where: [{mail: form.mail}]
-        };
-
-        datos.Usuario.findOne(filtro)
-        .then((result) => {
-
-            if (result == null) return res.send("No existe el mail " +  form.mail)
-            
-
-            let check = bcrypt.compareSync(form.contrasenia, result.contrasenia);
-            if (check) {
-                req.session.user = result;
-
-                /* que lo guarde en cookie si el usuario lo tildo */
-                if (form.rememberme != undefined) {
-                    res.cookie("userId", result.id, {maxAge: 1000 * 60 * 15});
-                }
-                return res.redirect("/product");
-            } else {
-                return res.send("La contraseña es incorrecta")
-            }
-            
-           
-
-        }).catch((err) => {
-            return console.log(err);
-        });
-
-        // res.render('search-results', {datos : datos})
+        res.render('search-results', {datos : datos})
     },
     add : function(req,res){
         res.render('product-add', {datos : datos})
